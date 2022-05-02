e30a151c9f9210f330572ced2a8adabd61190191
David Saavedra <davideloymartinez@gmail.com>
2022-05-02 16:12:55 -0700

    second try to add

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ import requests
 
 def cocktail(drink):
     response = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}")
-    # https://www.thecocktaildb.com/api/json/v1/1/random.php
     # print(response.status_code)
     return response.json()
 
