43f5915012acd3f9d8a5daadcdbad5e0db1100e8
David Saavedra <davideloymartinez@gmail.com>
2022-05-02 15:04:46 -0700

    David: Api call to cocktaildb

---
+++ api.py
@@ -0,0 +1,9 @@
+import requests
+
+def cocktail(drink):
+    response = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}")
+    # https://www.thecocktaildb.com/api/json/v1/1/random.php
+    # print(response.status_code)
+    return response.json()
+
+print(cocktail('margarita'))
