704c1ee00b3a598d842e8aba882aa85f7248b154
Ty <tjharo2@gmail.com>
2022-05-03 16:26:10 -0700

    Prints name of a random drink and all ingredients of that drink

--- 205_final_project.py
+++ 205_final_project.py
@@ -1,6 +1,6 @@
-#Final Project Starter File Test 1 Test 2
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap4
+from api import *
 
 app = Flask(__name__)
 bootstrap = Bootstrap4(app)
@@ -8,3 +8,11 @@ bootstrap = Bootstrap4(app)
 @app.route('/')
 def home():
     return render_template('home.html')
+
+random_drink = random_drink()
+print(random_drink['drinks'][0]['strDrink']) # print name of drink
+print('-------------')
+for i in range(15): # 1-15 is all possible ingredients in each beverage
+    ingredient = random_drink['drinks'][0]['strIngredient'+str(i+1)]
+    if ingredient != None:
+        print(ingredient)
--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 import requests
 
 def cocktail(drink):
-    response = requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}")
-    # https://www.thecocktaildb.com/api/json/v1/1/random.php
-    # print(response.status_code)
-    return response.json()
+    return requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}").json()
+
+def random_drink():
+    return requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/random.php").json()
