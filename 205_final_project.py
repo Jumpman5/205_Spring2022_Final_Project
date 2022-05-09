d4085511b5e594fba813a4a7e94dc4f3d6cacaa1
David Saavedra <davideloymartinez@gmail.com>
2022-05-09 00:20:03 -0700

    prices are added from amazon fresh

--- 205_final_project.py
+++ 205_final_project.py
@@ -13,19 +13,32 @@ def home():
 def drink():
     if(request.method == 'POST'):  # used for when user wants specific drink
         cocktail = cocktailByName(request.form.get("cocktailName")) # example margarita
-        return render_template('drink.html', cocktail = cocktail)
+
+        ingPrices = [] # prices of the ingredients
+        ing = [] # ingredients
+        for i in range(15): # 1-15 is all possible ingredients in each beverage
+            ingredient = cocktail['drinks'][0]['strIngredient'+str(i+1)]
+            if ingredient != None:
+                ingPrices.append(getPrice(ingredient))
+                ing.append(ingredient)
+            else:
+                break
+
+        return render_template('drink.html', cocktail = cocktail, prices=ingPrices)
     else: # assumes user hit random, since "Random Drink" buttom or a tag is not in form tag
         cocktail = random_drink()
-        return render_template('drink.html', cocktail = cocktail)
-    
 
-# random_drink = random_drink()
-# print(random_drink['drinks'][0]['strDrink']) # print name of drink
-# print('-------------')
-# for i in range(15): # 1-15 is all possible ingredients in each beverage
-#     ingredient = random_drink['drinks'][0]['strIngredient'+str(i+1)]
-#     if ingredient != None:
-#         print(ingredient)
+        ingPrices = [] # prices of the ingredients
+        ing = [] # ingredients
+        for i in range(15): # 1-15 is all possible ingredients in each beverage
+            ingredient = cocktail['drinks'][0]['strIngredient'+str(i+1)]
+            if ingredient != None:
+                ingPrices.append(getPrice(ingredient))
+                ing.append(ingredient)
+            else:
+                break
+
+        return render_template('drink.html', cocktail = cocktail, prices=ingPrices)
 
 '''
 how to run
--- api.py
+++ api.py
@@ -1,7 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
 def cocktailByName(drink):
     return requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drink}").json()
 
 def random_drink():
     return requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/random.php").json()
+
+def getPrice(item): # gets first result price from amazon fresh
+    HEADERS = ({'User-Agent':
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+            'Accept-Language': 'en-US, en;q=0.5'})
+    URL = f"https://www.amazon.com/s?k={item}&i=amazonfresh"
+    webpage = requests.get(URL, headers=HEADERS)
+    soup = BeautifulSoup(webpage.content, "lxml")
+    title = soup.find("span", attrs={"class":'a-offscreen'}).string
+    return title
+    
