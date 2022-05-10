31172b873a2d1a9729aea691af400cf0ddb96a38
David Saavedra <davideloymartinez@gmail.com>
2022-05-09 23:33:57 -0700

    added cookies to api to make amazon work again

--- 205_final_project.py
+++ 205_final_project.py
@@ -1,27 +1,6 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
 from api import *
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from distutils.filelist import findall
-import ssl
-#ssl._create_default_https_context = ssl._create_unverified_context
-#my_site='https://www.airnow.gov/?city=Monterey&state=CA&country=USA'
-#site_html=urlopen(my_site)
-# print out a portion of the HTML
-#print(site_html.read()[5000:5400])
-target_html = urlopen('https://www.target.com/s?searchTerm=almond+milk')
-#print(target_html.read()[0:10000])
-data = BeautifulSoup(target_html, 'lxml')
-
-#for i in data.findAll('img'):
-    #print(i.attrs['src'])
-
-#for i in data.findAll('span'):
-    #print(i.attrs['class'])
-#use beautiful soup for reading specific tags like <span>$3.19</span>
-# print(cocktailRandom())
-# print(cocktail('margarita'))
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -30,26 +9,16 @@ bootstrap = Bootstrap5(app)
 def home():
     return render_template('home.html')
 
-#random_drink = random_drink()
-#print(random_drink['drinks'][0]['strDrink']) # print name of drink
-#print('-------------')
-#for i in range(15): # 1-15 is all possible ingredients in each beverage
-    #ingredient = random_drink['drinks'][0]['strIngredient'+str(i+1)]
-    #if ingredient != None:
-        #print(ingredient)
-        #1
 @app.route('/drink', methods=['POST', 'GET'])
 def drink():
     if(request.method == 'POST'):  # used for when user wants specific drink
         cocktail = cocktailByName(request.form.get("cocktailName")) # example margarita
 
         ingPrices = [] # prices of the ingredients
-        ing = [] # ingredients
         for i in range(15): # 1-15 is all possible ingredients in each beverage
             ingredient = cocktail['drinks'][0]['strIngredient'+str(i+1)]
             if ingredient != None:
                 ingPrices.append(getPrice(ingredient))
-                ing.append(ingredient)
             else:
                 break
 
@@ -58,21 +27,19 @@ def drink():
         cocktail = random_drink()
 
         ingPrices = [] # prices of the ingredients
-        ing = [] # ingredients
         for i in range(15): # 1-15 is all possible ingredients in each beverage
             ingredient = cocktail['drinks'][0]['strIngredient'+str(i+1)]
             if ingredient != None:
                 ingPrices.append(getPrice(ingredient))
-                ing.append(ingredient)
             else:
                 break
 
         return render_template('drink.html', cocktail = cocktail, prices=ingPrices)
 
-
-
-#how to run
-#$env:FLASK_APP = "205_final_project.py"
-#$env:FLASK_DEBUG = "1"
-#flask run
-#ctl + c
+'''
+how to run
+$env:FLASK_APP = "205_final_project.py"
+$env:FLASK_DEBUG = "1"
+flask run
+ctl + c
+'''
--- api.py
+++ api.py
@@ -8,12 +8,18 @@ def random_drink():
     return requests.get(f"https://www.thecocktaildb.com/api/json/v1/1/random.php").json()
 
 def getPrice(item): # gets first result price from amazon fresh
-    HEADERS = ({'User-Agent':
-            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
-            'Accept-Language': 'en-US, en;q=0.5'})
-    URL = f"https://www.amazon.com/s?k={item}&i=amazonfresh"
-    webpage = requests.get(URL, headers=HEADERS)
+    cookies = {'session': '131-1062572-6801904'} # need this now ???? but it works now originally 131-1062572-6801905
+    # header = { # keep just in case
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+    # }
+    header = ({'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Accept-Language': 'en-US, en;q=0.5'})
+    url = f"https://www.amazon.com/s?k={item}&i=amazonfresh"
+    print(url)
+    webpage = requests.get(url, headers=header, cookies=cookies)
     soup = BeautifulSoup(webpage.content, "lxml")
+
     title = "Price information not found"
     found = soup.find("span", attrs={"class":'a-offscreen'})
     if found:
